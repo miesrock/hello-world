9ed581c40243f39312fda2c750613e3e376a8cff
miesrock <hezaoyi@foxmail.com>
2018-02-22 22:06:43 +0800

    写了个文件头

---
+++ 2048_20180222byHe/2048_20180222byHe/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+#include <conio.h>
+
+using namespace std;
+
+int g_arrays[4][4] = {};
+void left();
+void right();
+void up();
+void down();
+void _2048game();
+
+void main()
+{
+
+	system("pause");
+}
+
+
